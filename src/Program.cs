342a5d33b94329166128e6fae930aa85ca5e3288
Paige Seivertson <pzeig66@yahoo.com>
2020-11-22 22:48:45 -0500

    Init commit

---
+++ src/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace Discord_LaTeX_Bot
+{
+    class Program
+    {
+        private static DiscordClient Client;
+
+        static void Main(string[] args)
+        {
+            if (!File.Exists("id.txt"))
+                throw new Exception("Bruh.");
+
+            Client = new DiscordClient(new DiscordConfiguration { Token = File.ReadAllText("id.txt") });
+
+            Client.MessageCreated += (MessageCreateEventArgs e) =>
+            {
+                if (e.Author.IsBot)
+                    return Task.CompletedTask;
+
+                var m = e.Message;
+                string mess = m.Content;
+
+                if (mess == "BRUH")
+                    m.Channel.SendMessageAsync("TOAST");
+
+                return Task.CompletedTask;
+            };
+
+            Client.ConnectAsync();
+
+            SpinWait.SpinUntil(() => false); // this is a hack to stop the program from self closing
+        }
+    }
+}
