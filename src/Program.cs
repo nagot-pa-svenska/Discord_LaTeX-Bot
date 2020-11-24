12075a0c66bd15ab0a6be39076ea9d0c6ac714c1
Fractal Hassan <awesomelightning@gmail.com>
2020-11-23 19:20:36 -0500

    init commit

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,14 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 using DSharpPlus;
 using DSharpPlus.EventArgs;
+using DSharpPlus.Entities;
+
+using Octave.NET;
 
 namespace Discord_LaTeX_Bot
 {
@@ -13,7 +19,7 @@ namespace Discord_LaTeX_Bot
 
         static void Main(string[] args)
         {
-            if (!File.Exists("id.txt"))
+            if (!File.Exists("id.txt")) 
                 throw new Exception("Bruh.");
 
             Client = new DiscordClient(new DiscordConfiguration { Token = File.ReadAllText("id.txt") });
@@ -26,11 +32,42 @@ namespace Discord_LaTeX_Bot
                 var m = e.Message;
                 string mess = m.Content;
 
-                if (mess == "BRUH")
-                    m.Channel.SendMessageAsync("TOAST");
+                if (mess.ToLower() == "pog" || mess.ToLower() == "poggers")
+                {
+                    DiscordEmoji lyra = DiscordEmoji.FromName(Client,":lyrapog:");
+                    DiscordEmoji paige = DiscordEmoji.FromName(Client, ":paigepog:");
+                    DiscordEmoji fractal = DiscordEmoji.FromName(Client, ":fractalpog:");
+                    DiscordEmoji ikea = DiscordEmoji.FromName(Client, ":ikeapog:");
+                    m.Channel.SendMessageAsync(lyra + paige + fractal + ikea);
+                }
+
+                if (mess == "2+2")
+                {
+                    using (var octave = new OctaveContext())
+                    {
+                        var scalarResult = octave
+                           .Execute(mess)
+                           .AsScalar();
+                           
+                        m.Channel.SendMessageAsync(scalarResult.ToString());
+                    }
+                }
+
+                char[] fourFunction = { '+', '-', 'x', '*', '/' };
+                LinkedList <String> nums = new LinkedList<String>();
+                LinkedList<String> ops = new LinkedList<String>();
+                
+                for(int i = 0; i < mess.Length; i++)
+                {
+                    // TODO: use regex.split to tokenize message input into strings->double in nums, and operators into ops.
+                    // use stringbuilder to "build" a number until it hits a delimiter? seems workaround and slow. does c# have stringbuilder?
+                    // stringbuild until hit delimiter, unload to linked list, getchar, repeat until endchar
+                    
+                }
 
                 return Task.CompletedTask;
             };
+
 
             Client.ConnectAsync();
 
