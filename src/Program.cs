056bb9675256547a7d073cbfab020f37de108598
Paige Seivertson <pzeig66@yahoo.com>
2020-12-03 01:35:37 -0500

    Rough implementation of getting ops and nums

--- src/Program.cs
+++ src/Program.cs
@@ -46,29 +46,34 @@ namespace Discord_LaTeX_Bot
 
                     m.Channel.SendMessageAsync(lyra + paige + fractal + ikea);
                 }
+                else if (!mess.StartsWith("!latex"))
+                    return Task.CompletedTask;
 
-                if (mess == "2+2")
+                mess = mess.Substring("!latex ".Length);
+
+                char[] fourFunctions = { '+', '-', 'x', '*', '/' };
+                //LinkedList<double> nums = new LinkedList<double>();
+                //LinkedList<string> ops = new LinkedList<string>();
+
+                var nums = mess.Split(fourFunctions);
+                // minus one on length to avoid OutOfBoundsException caused by grabbing the next entry
+                string[] ops = new string[nums.Length - 1];
+
+                string equation = "";
+
+                for (int i = 0; i < ops.Length; i++)
                 {
-                    using (var octave = new OctaveContext())
-                    {
-                        var scalarResult = octave
-                           .Execute(mess)
-                           .AsScalar();
-
-                        m.Channel.SendMessageAsync(scalarResult.ToString());
-                    }
+                    ops[i] = ReturnInbetween(mess, nums[i], nums[i + 1]);
+                    equation += nums[i] + ops[i] + nums[i + 1];
                 }
 
-                char[] fourFunction = { '+', '-', 'x', '*', '/' };
-                LinkedList<String> nums = new LinkedList<String>();
-                LinkedList<String> ops = new LinkedList<String>();
+                m.Channel.SendMessageAsync(new OctaveContext().Execute(equation).AsScalar().ToString());
 
                 for (int i = 0; i < mess.Length; i++)
                 {
                     // TODO: use regex.split to tokenize message input into strings->double in nums, and operators into ops.
                     // use stringbuilder to "build" a number until it hits a delimiter? seems workaround and slow. does c# have stringbuilder?
                     // stringbuild until hit delimiter, unload to linked list, getchar, repeat until endchar
-
                 }
 
                 return Task.CompletedTask;
@@ -77,6 +82,18 @@ namespace Discord_LaTeX_Bot
             Client.ConnectAsync();
 
             SpinWait.SpinUntil(() => false); // this is a hack to stop the program from self closing
+        }
+
+        /// <summary>
+        /// Finds a string between two given strings.
+        /// Used when you don't know what will be in-between two strings, but know what strings are around it.
+        /// </summary>
+        /// <param name="excerpt">The string you wish to search within.</param>
+        /// <param name="before">The string located before the string you want.</param>
+        /// <param name="after">The string located after the string you want.</param>
+        private static string ReturnInbetween(string excerpt, string before, string after)
+        {
+            return excerpt.Substring(excerpt.IndexOf(before) + 1, excerpt.IndexOf(after, excerpt.IndexOf(before) + 1) - (excerpt.IndexOf(before) + 1));
         }
     }
 }
